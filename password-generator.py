62a6698697328c533f19e1300e509428f66bf385
Thom Versigny <Thomversigny@hotmail.com>
2022-04-04 11:20:31 +0200

    password generator toegevoegt

--- password-generator.py
+++ password-generator.py
@@ -1,24 +1,26 @@
 import random
 import string
-characters = list(string.ascii_letters + string.digits + "@#$%&_?")
 letters = string.ascii_lowercase
 hooftletters = string.ascii_uppercase
 getal = string.digits
 speciale = ['@', '#', '$', '%', '&', '_' ,'?']
 
 def wachtwoord():
-    random.shuffle(characters)
     password = []
-    for i in range(24):
-	    password.append(random.choice(characters))
-    
-    print("".join(password))
-    if password.count(speciale) == 0 or password.count(speciale) == 23:
-        random.shuffle(password)
-    else:
-        if password.count(getal) == 0 or password.count(getal) == 1 or password.count(getal) == 2:
-            random.shuffle(password)
-        else:
+    for i in range(random.randint(2,6)):
+        password.append(random.choice(hooftletters))
+    for i in range(3):
+        password.append(random.choice(speciale))
+    for i in range(random.randint(4, 7)):
+        password.append(random.choice(getal))
+    x = 24 - len(password)
+    for i in range(x):
+        password.append(random.choice(letters))
+    random.shuffle(password)
+    while (password[0] in speciale or password[0] in getal or password[1] in getal or password[2] in getal or password[23] in speciale):
+         random.shuffle(password)
+    return ''.join(password)
 
-
+password = wachtwoord()
+print(password)
 
